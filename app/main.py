dbf7f0adab46cedaa0ae8363f6b860a9f42fa410
murphym2004 <150245337+murphym2004@users.noreply.github.com>
2026-09-19 14:34:02 +0000

    added get users by ID and Delete users by ID to main

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def hello():
 @app.post("/api/users", status_code=status.HTTP_201_CREATED)
 def add_user(new_user: UserCreate):
     for existing_user in users:
-        if existing_user.user_id == new_user.new_id:
+        if existing_user.user_id == new_user.user_id:
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="a user with this id already exists")
     users.append(new_user)
     return new_user
@@ -24,3 +24,17 @@ def add_user(new_user: UserCreate):
 @app.get("/api/user")
 def get_users():
     return users
+
+@app.get("/api/user/{user_id}")
+def get_user(user_id: int):
+    for user in users:
+        if user.user_id == user_id:
+            return user
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
+
+@app.delete("/api/user/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(user_id: int):
+    global users
+    users = [user for user in users if user.user_id != user_id]
+    if user_id not in [user.user_id for user in users]:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
--- app/schema.py
+++ app/schema.py
@@ -1,9 +1,9 @@
 from typing import Annotated
-from pydantic import BaseModel, EmailStr , field, StringConstraints
+from pydantic import BaseModel, EmailStr , Field, StringConstraints
 
 class UserCreate(BaseModel):
-    uder_id: int = field(gt=0)
+    user_id: int = Field(gt=0)
     name: Annotated[str, StringConstraints(min_length=2, max_length=50)]
     email: EmailStr
-    age: int = field(gt=18, lt=120)
+    age: int = Field(gt=18, lt=120)
     student_id: Annotated[str, StringConstraints(pattern=r"^\d{7}$")]
