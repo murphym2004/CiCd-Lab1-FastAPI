4348b406716cbe3c7383e79168fbbd1b28c002b4
murphym2004 <150245337+murphym2004@users.noreply.github.com>
2026-09-25 12:22:59 +0000

    test for missing users

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,3 +1,5 @@
+from urllib import response
+
 import pytest
 
 def user_payload(uid=1, name="paul", email="paul@atu.ie", age=26, student_id="1234567"):
@@ -53,5 +55,10 @@ def test_get_existing_user_returns_200(client):
 
     assert response.status_code == 200
     assert response.json()["user_id"] == 11
+
+def test_get_missing_user_returns_404(client):
+    response = client.get("/api/users/999")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "User not found"
 
 
